a05af5a8158dbeaeff5f0be37e82f6f0b1424e15
Nethanel Bedok <nethanelbedok@gmail.com>
2022-12-18 11:56:12 +0200

    jjj

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,25 @@ import { Api } from './api/api.js';
 function App() {
   const [user,setuser]=useState()
   useEffect(()=>{
-    Api.get('/users')
-    .then(({data})=>{
-console.log(data)
-setuser(data);
-
-    })
-    .catch((err)=>console.log(err))
+    try{
+      Api.get('/users')
+      .then(({data})=>{
+  console.log(data)
+  setuser(data);
+  
+})
+    }
+  
+    catch(err){
+      console.log(err);
+    }
 
     
   },[])
   
   return (
     <div className="App">
-     { user?.map((item,key)=><div key={item._id}><h1>cash : {item.cash} credit:{item.credit}</h1>  </div> )}
+     { user?.map((item,key)=> <div key={item._id}><h1>cash : {item.cash} credit:{item.credit}</h1>  </div> )}
     </div>
   );
 }
