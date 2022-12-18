934e8f1c6749bb3117b7aa4dcc6a9f5ac4601535
Nethanel Bedok <nethanelbedok@gmail.com>
2022-12-18 09:55:51 +0200

    kk

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 let myurl='http://localhost:7000/api/bankapi';
+if (process.env.NODE_ENV === 'production') {
+    myurl = 'bankapi ';
+  }
+  
 export const Api=axios.create({
     baseURL:myurl,
 });
